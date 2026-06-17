d9b4303b419943b7d9792092a05b686b30b4b5bb
Fabio Afonso <fabioafonsoofc@gmail.com>
2026-06-17 16:28:59 -0300

    fix allowed hosts

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-29@!c5go_4gk-v$n!s+bpb_pa%%6@3=gkj(8e6ohc-o$ycu+mi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -144,7 +144,7 @@ SECRET_KEY = 'django-insecure-29@!c5go_4gk-v$n!s+bpb_pa%%6@3=gkj(8e6ohc-o$ycu+mi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
